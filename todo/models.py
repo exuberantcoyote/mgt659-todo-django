ae2c4b2a64c06065522a5094aa85053c088cd448
exuberantcoyote <trenttolley@gmail.com>
2016-02-15 18:39:54 +0000

    Added task handling functionality

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    email = models.EmailField(max_length=50, unique='true')
+    email = models.EmailField(max_length=50, unique=True)
     name = models.CharField(max_length=50)
     hashPassword = models.CharField(max_length=50)
 
@@ -13,13 +13,13 @@ class User(models.Model):
 
 class Task(models.Model):
     owner = models.IntegerField()
-    isOwner = models.BooleanField(default='false')
+    isOwner = models.BooleanField(default=False)
     title = models.CharField(max_length=5000)
-    description = models.CharField(max_length=5000, blank='true')
-    isComplete = models.BooleanField(default='false')
-    collaborator1 = models.EmailField(max_length=50, blank='true')
-    collaborator2 = models.EmailField(max_length=50, blank='true')
-    collaborator3 = models.EmailField(max_length=50, blank='true')
+    description = models.CharField(max_length=5000, blank=True)
+    isComplete = models.BooleanField(default=False)
+    collaborator1 = models.EmailField(max_length=50, blank=True)
+    collaborator2 = models.EmailField(max_length=50, blank=True)
+    collaborator3 = models.EmailField(max_length=50, blank=True)
 
     def _str_(self):
         return self.title
